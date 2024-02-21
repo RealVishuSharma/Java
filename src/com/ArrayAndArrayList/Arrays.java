1e0efa779623c02f3556eae29330f67ad685f304
RealVishuSharma <vishusharmaa2211@gmail.com>
2024-02-21 22:43:28 +0530

    Added Some files related to Arrays.

---
+++ src/com/ArrayAndArrayList/Arrays.java
@@ -0,0 +1,12 @@
+package com.ArrayAndArrayList;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int[] roll = new int[5];
+
+    }
+}

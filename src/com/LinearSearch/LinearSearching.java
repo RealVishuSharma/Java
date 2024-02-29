d400cfd715c8f7877ce9cb80ed86bd0fdef8c194
RealVishuSharma <vishusharmaa2211@gmail.com>
2024-02-29 22:26:29 +0530

    Added sone files.

--- src/com/LinearSearch/LinearSearching.java
+++ src/com/LinearSearch/LinearSearching.java
@@ -1,7 +1,36 @@
 package com.LinearSearch;
 
+import java.util.Scanner;
+
 public class LinearSearching {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        int[] array = new int[5];
+        System.out.println("Enter the Array: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+
+        System.out.println("Enter the element you want to find: ");
+        int tar = input.nextInt();
+
+        int res = LinearSearch(array, tar);
+        System.out.println("The element is present in the array at index no." + res);
+    }
+
+    static int LinearSearch(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int element = arr[i];
+            if (element == target) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }

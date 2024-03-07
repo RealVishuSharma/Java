78032af5541ee231a58bbfd6f3001f119551194a
RealVishuSharma <vishusharmaa2211@gmail.com>
2024-03-08 00:47:06 +0530

    Added problems realted to Binary Searching.

---
+++ src/com/BinarySearch/FloorValue.java
@@ -0,0 +1,47 @@
+package com.BinarySearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FloorValue {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the length of the Array: ");
+        int length = input.nextInt();
+
+        int[] array = new int[length];
+
+        System.out.println("Enter the elements of the Array: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+
+        System.out.println("Enter the element you want to Search: ");
+        int element = input.nextInt();
+
+        System.out.println("The Current Array is: ");
+        System.out.println(Arrays.toString(array));
+
+        int output = Search(array, element);
+        System.out.println("The floor of the given value is: " + output);
+    }
+
+    static int Search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = 0;
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else{
+                return mid;
+            }
+        }
+        return arr[mid - 1];
+    }
+}

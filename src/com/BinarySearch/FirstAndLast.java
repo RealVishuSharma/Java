7904f743b9f11589a9d68dc8f82f0e56883cbfaf
RealVishuSharma <vishusharmaa2211@gmail.com>
2024-03-19 17:04:44 +0530

    Added new programs.

--- src/com/BinarySearch/FirstAndLast.java
+++ src/com/BinarySearch/FirstAndLast.java
@@ -12,6 +12,7 @@ public class FirstAndLast {
 
         int[] nums = new int[length];
 
+        System.out.println("Enter the elements of the Array: ");
         for (int i = 0; i < nums.length; i++) {
             nums[i] = input.nextInt();
         }
@@ -22,23 +23,44 @@ public class FirstAndLast {
         System.out.println("The Current Array is: ");
         System.out.println(Arrays.toString(nums));
 
-        TimesFound(nums, element);
+        int[] output = TimesFound(nums, element);
+        System.out.println("The first and last occurrence of " + element + " is: " + Arrays.toString(output));
     }
 
-    static int TimesFound(int[] arr, int element) {
+    static int[] TimesFound(int[] arr, int element) {
+        int[] ans = {-1, -1};
+
+        int first = search(arr, element, true);
+        int last = search(arr, element, false);
+
+        ans[0] = first;
+        ans[1] = last;
+
+        return ans;
+    }
+
+    static int search(int[] nums, int target, boolean found) {
+        int ans = -1;
         int start = 0;
-        int end = arr.length - 1;
+        int end = nums.length - 1;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            if (element < arr[mid]) {
+            if (target < nums[mid]) {
                 end = mid - 1;
-            } else if (element > arr[mid]) {
+            } else if (target > nums[mid]) {
                 start = mid + 1;
             } else {
-                
+               ans = mid;
+               if (found) {
+                   end = mid -1;
+               }else{
+                   start = mid + 1;
+               }
             }
         }
+
+        return ans;
     }
 }
